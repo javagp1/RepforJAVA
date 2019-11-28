cf979c566a8da5a1415d9cac4b2bd555056ad3ca
YXG11java <Yxingguo12@163.com>
2019-11-28 09:13:18 +0800

    杨兴国上传11.28

---
+++ springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/controller/GoodscollectionCtrl.java
@@ -0,0 +1,5 @@
+package cn.fx.jlx.springMVC.springMVC.controller;
+
+public class GoodscollectionCtrl {
+
+}
---
+++ springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/service/GoodsCollectionService.java
@@ -0,0 +1,37 @@
+package cn.fx.jlx.springMVC.springMVC.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import cn.fx.jlx.springMVC.springMVC.mapper.GoodscollectionMapper;
+import cn.fx.jlx.springMVC.springMVC.mapper.GoodscollectionMapperEx;
+import cn.fx.jlx.springMVC.springMVC.pojo.GoodscollectionKey;
+
+@Service
+public class GoodsCollectionService {
+	@Autowired
+	private GoodscollectionMapper goodscollectionMapper;
+	
+	@Autowired
+	private GoodscollectionMapperEx goodscollectionmapperex;
+
+	@Transactional
+	public boolean addcollection(GoodscollectionKey goodscollection){
+		return goodscollectionMapper.insert(goodscollection)==1;
+	}
+	
+	@Transactional
+	public boolean deleteCollection(GoodscollectionKey goodscollectionKey){
+		return goodscollectionMapper.deleteByPrimaryKey(goodscollectionKey)==1;
+	}
+	
+	@Transactional
+	public List<Map> selectCollection(Integer userid){
+	 return	goodscollectionmapperex.getCollections(userid);
+	}
+	
+}
