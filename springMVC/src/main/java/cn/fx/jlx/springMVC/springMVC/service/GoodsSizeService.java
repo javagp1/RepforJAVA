e82052b01a3bd6eb4d31fcc96f891297934d5353
ren981007 <ren981007@126.com>
2019-12-02 10:42:27 +0800

    uu

--- springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/service/GoodsSizeService.java
+++ springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/service/GoodsSizeService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 import cn.fx.jlx.springMVC.springMVC.mapper.GoodssizeMapper;
 import cn.fx.jlx.springMVC.springMVC.pojo.Goodssize;
 import cn.fx.jlx.springMVC.springMVC.pojo.GoodssizeExample;
@@ -14,6 +15,7 @@ public class GoodsSizeService {
 	
 	@Autowired
 	private GoodssizeMapper goodssizeMapper;
+	
 
 	public List<Goodssize> getAllGoodsSize() {
 
--- springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/service/OrderInfoService.java
+++ springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/service/OrderInfoService.java
@@ -48,11 +48,11 @@ public class OrderInfoService {
 	@Transactional
 	public void modifystate(String orderInfoID){
 		Orderinfo order=orderinfoMapper.selectByPrimaryKey(orderInfoID);	
-		order.setOfstate(3);
+		order.setOfstate(4);
 		orderinfoMapper.updateByPrimaryKeySelective(order);		
 		OrderlistExample example=new OrderlistExample();
 		example.createCriteria().andOfidEqualTo(orderInfoID);
-		List<Orderlist> ol_list= orderlistMapper.selectByExample(example);				
+		
 							
 	}
 	
@@ -65,7 +65,7 @@ public class OrderInfoService {
 	 */
 	public List<Orderlist> getOrderInformationsByStuserID(Integer stuserid){	
 		OrderlistExample example=new OrderlistExample();
-		example.createCriteria().andGdidEqualTo(stuserid);
+		example.createCriteria().andGdidEqualTo(stuserid).andOlstatusEqualTo(2);
 		return orderlistMapperEx.getallstorderlist(stuserid);
 	}
 	
@@ -79,7 +79,7 @@ public class OrderInfoService {
 		List<Orderlist> list = orderlistMapper.selectByExample(example );
 		
 		for (Orderlist orderlist : list) {
-			orderlist.setOlstatus(2);
+			orderlist.setOlstatus(4);
 			orderlistMapper.updateByPrimaryKeySelective(orderlist);
 		}
 		Orderinfo orderinfo = orderinfoMapper.selectByPrimaryKey(ofid);
