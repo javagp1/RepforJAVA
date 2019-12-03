420e6cf1b75bc94bcabd4d82c4b0baa2a7603e78
ljq0225 <ljq19990225@126.com>
2019-12-03 18:27:41 +0800

    上传

--- springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/controller/FileControler.java
+++ springMVC/src/main/java/cn/fx/jlx/springMVC/springMVC/controller/FileControler.java
@@ -15,22 +15,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileControler {
 
 	@PostMapping("/filecontrollerupload")
-	public String  upload(@RequestParam(value="file1") MultipartFile[] file1 ) throws IOException {
+	public Object upload(@RequestParam(value="file1") MultipartFile[] file1 ) throws IOException {
 			
 		for (MultipartFile f : file1) {
-			String old_fileName=f.getOriginalFilename();
-			
-			StringBuffer fileName=new StringBuffer(UUID.randomUUID().toString());
-					
-						 fileName.append(old_fileName.substring(old_fileName.lastIndexOf(".")));
-							
-//						    //创建放置在服务器本地的文件
-						    File localFile = new File("d:"+File.separator+"sysfiles190806"+File.separator+"uploadfiles"+File.separator+fileName);
-						    //把传上来的文件写到本地文件
-						    f.transferTo(localFile);
+			String old_fileName=f.getOriginalFilename();				
+		    //创建放置在服务器本地的文件
+		   File localFile = new File("d:"+File.separator+"sysfiles190806"+File.separator+"uploadfiles"+File.separator+file1);
+		   //把传上来的文件写到本地文件
+	        f.transferTo(localFile);
 		}
 	    
-	    return "/upload_success.html";
+	    return true;
 	}
 	
 	
